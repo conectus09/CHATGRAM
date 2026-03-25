566c39d38f24def247be5c1ed5aba50e9593af52
harsh <conectus09@gmail.com>
2026-03-25 16:50:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -326,6 +326,8 @@ io.on('connection', (socket) => {
     }
   });
 });
-
+app.get('/', (req, res) => {
+  res.send('🚀 BUMPY Backend is Running Successfully');
+});
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
